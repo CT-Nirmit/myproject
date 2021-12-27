0f5c466ad10b3110f49eec322c722093af76cbfd
CT-Nirmit <nirmit.pandagre@cubictree.com>
2021-12-27 12:56:12 +0000

    scapping code

---
+++ test.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+url = "https://www.ruby-lang.org/"
+uri = URI.parse(url)
+response = Net::HTTP.get_response(uri)
+
+begin
+  file = File.open("/home/ubuntu/Downloads/newfile.txt", "w")
+  file.write(response) 
+  puts "Done"
+rescue IOError => e
+  puts e
+ensure
+  file.close unless file.nil?
+end
+
